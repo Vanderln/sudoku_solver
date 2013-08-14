8d464bdad599896a2e6a1149faefeb4939036e07
Bill Van Der Laan <wvanderln@hotmail.com>
2013-08-14 11:15:01 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SudokuSolver::Application.config.secret_key_base = 'f8f7c9ccb200822bd5f8069c72f27d627584c620cd03f5c3f65cf54e4fae8777d48a74d2e0823444fce0f930f27a78eb22e81aa58df09d3c432a712d1c4ebe1a'
